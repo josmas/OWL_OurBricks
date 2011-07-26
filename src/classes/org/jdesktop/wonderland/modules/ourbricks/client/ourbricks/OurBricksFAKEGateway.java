df2ee2a3a227e00ec9b8a17bb2df6638d56393fe
jos <jos@tchpc.tcd.ie>
2011-07-26 13:04:31 +0100

    Moving the fake gateway to the classes folder to be ablr to use it with the JPanel while on development... not a good thing to do but OK for now

--- src/classes/org/jdesktop/wonderland/modules/ourbricks/client/ourbricks/OurBricksFAKEGateway.java
+++ src/classes/org/jdesktop/wonderland/modules/ourbricks/client/ourbricks/OurBricksFAKEGateway.java
@@ -11,6 +11,34 @@ import java.net.URL;
  */
 public class OurBricksFAKEGateway implements OurBricksGateway {
 
+    //TODO there is repetition here and in the Test classes; think aobut how
+    // avoid that
+    private String JSONresponse = "" +
+                " {   \"price\": null, " +
+                " \"thumbnail_link\": \"http://vu.ourbricks.com/6e2dd650eaa7832d88f4e8b70e3db611/processed/ourbricksThumb.jpg\", " +
+                " \"username\": \"danx0r\", " +
+                " \"description\": \"testing navigation\", " +
+                " \"download_link\": \"http://vu.ourbricks.com/6e2dd650eaa7832d88f4e8b70e3db611/processed/ourbricks.zip\", " +
+                " \"title\": \"kataspace main scene\", " +
+                " \"viewer_link\": \"http://ourbricks.com/viewer/6e2dd650eaa7832d88f4e8b70e3db611\", " +
+                " \"author\": \"\", " +
+                " \"license_code\": \"ccby\", " +
+                " \"id\": \"6e2dd650eaa7832d88f4e8b70e3db611\" " +
+                " } ";
+
+
+    private String JSONListResponse =
+
+            " { " +
+                " \"prev_start\": null, " +
+                " \"items\": [ " +
+                JSONresponse +
+                " , " +
+                JSONresponse +
+                " ], " +
+                " \"next_start\": 1 " +
+            " } ";
+
     /**
      * A FAKE request to avoid contacting the external service. A simple string
      * reply is expected
@@ -21,7 +49,7 @@ public class OurBricksFAKEGateway implements OurBricksGateway {
      */
     private String ourBricksGETConnection(URL remoteURL) throws MalformedURLException, IOException {
         
-        return OurBricksListTest.JSONListResponse;
+        return JSONListResponse;
     }
 
     /**

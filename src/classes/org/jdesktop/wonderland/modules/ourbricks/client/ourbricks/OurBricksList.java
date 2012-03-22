7172da1a1a14529118a1b20ea480683fa93f9594
Josmas Flores <josmasflores@gmail.com>
2012-03-22 17:12:32 +0000

    clean up comments and added TODO tasks

--- src/classes/org/jdesktop/wonderland/modules/ourbricks/client/ourbricks/OurBricksList.java
+++ src/classes/org/jdesktop/wonderland/modules/ourbricks/client/ourbricks/OurBricksList.java
@@ -9,24 +9,6 @@ import java.util.List;
  * @author jos
  */
 
-// Output example
-//    {
-//        "prev_start": null,
-//        "items": [
-//            {   "price": null,
-//                "thumbnail_link": "http://vu.ourbricks.com/6e2dd650eaa7832d88f4e8b70e3db611/processed/ourbricksThumb.jpg",
-//                "username": "danx0r",
-//                "description": "testing navigation",
-//                "download_link": "http://vu.ourbricks.com/6e2dd650eaa7832d88f4e8b70e3db611/processed/ourbricks.zip",
-//                "title": "kataspace main scene",
-//                "viewer_link": "http://ourbricks.com/viewer/6e2dd650eaa7832d88f4e8b70e3db611",
-//                "author": "",
-//                "license_code": "ccby",
-//                "id": "6e2dd650eaa7832d88f4e8b70e3db611"
-//            }    ],
-//        "next_start": 1
-//    }
-
 public class OurBricksList {
 
     private Integer prev_start;
--- src/test/org/jdesktop/wonderland/modules/ourbricks/client/ourbricks/OurBricksCuratorTest.java
+++ src/test/org/jdesktop/wonderland/modules/ourbricks/client/ourbricks/OurBricksCuratorTest.java
@@ -14,42 +14,10 @@ public class OurBricksCuratorTest {
     @Before
     public void setUp() {
     }
-    
-    /**
-     * 
-     * returns
-            {
-                prev_start: null,
-                items: [
-                    {
-                        username: "Manfred van der Voort",
-                        description: "",
-                        price: null,
-                        thumbnail_link: "http://vu.ourbricks.com/3939a9dc288effbba7e6dee6df4495ff/processed/ourbricksThumb.jpg?133237015084",
-                        viewer_link: "http://ourbricks.com/viewer/3939a9dc288effbba7e6dee6df4495ff",
-                        id: "3939a9dc288effbba7e6dee6df4495ff",
-                        license_code: "allrightsreserved",
-                        technical_details: {
-                            num_normals: 4034553,
-                            num_triangles: 1344851,
-                            num_effects: 28,
-                            num_texcoords: 3357930,
-                            texture_ram: 4653056,
-                            draw_calls_raw: 3529,
-                            num_textures: 13,
-                            draw_calls_with_material_batching: 28,
-                            num_vertices: 4034553,
-                            draw_calls_with_instance_batching: 155
-                        },
-                        title: "Kolosseum",
-                        author: "Mario",
-                        download_link: null
-                    }
-                ],
-                next_start: 1
-                }
-    */
 
+    /**
+     * This is what the OurBricks API outputs now: March 2012
+     */
     private String JSONReply = 
             "{" +
             "prev_start: null," +
@@ -79,6 +47,7 @@ public class OurBricksCuratorTest {
             "}]," +
             "next_start: 1" +
             "}";
+
     private String JSONReplyTwo =
             "{" +
             "prev_start: 0," +
@@ -134,8 +103,6 @@ public class OurBricksCuratorTest {
             "next_start: 1" +
             "}";
     
-
-
     @Test
     public void should_return_empty_list() {
         OurBricksList curatedList;
